5a36f68f519235e16a72f76897c0d555f7878e9b
meatpotatoe <evan_a_s2@yahoo.com>
2016-10-24 20:23:18 -0700

    Update linkedlist.cpp

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -7,23 +7,22 @@ template<class Type>
 class linkedListType
 {
 public:
-	const linkedListType<Type>& operator=(const linkedListType<Type>&);
 	void initializeList();
 	bool isEmptyList() const;
 	void print() const;
 	int length() const;
 	void destroyList();
-	Type printTop() const;
-	Type printBot() const;
-	virtual bool search(const Type&) const = 0;
-	virtual void insertFirst(const Type&) = 0;
-	virtual void insertLast(const Type&) = 0;
-	virtual void deleteNode(const Type&) = 0;
+	Type Top() const;
+	Type Bot() const;
 	linkedList<Type> begin();
 	linkedList<Type> end();
+	virtual void insertFirst(const Type&);
+	virtual void insertLast(const Type&);
+	virtual void deleteNode(const Type&);
 	linkedListType();
 	linkedListType(const linkedListType<Type>&);
 	~linkedListType();
+	const linkedListType<Type>& operator=(const linkedListType<Type>&);
 protected:
 	int count;
 	nodeType<Type> *first;
@@ -31,37 +30,13 @@ protected:
 private:
 	void copyList(const linkedListType<Type>&);
 };
-
 template<class Type>
 bool linkedListType<Type>::isEmptyList() const
 {
-	return (first == NULL;)
-}
-
-template<class Type>
-linkedListType<Type>::linkedListType()
-{
-	first = NULL;
-	last = NULL;
-	count = 0;
-}
-template<class Type>
-void linkedListType<Type>::destroyList()
-{
-	nodeType<Type> *temp;
-	while (first != NULL)
-	{
-		temp = first;
-		first - first->link;
-		delete temp;
-	}
-	last = NULL;
-	count = 0;
-}
-template<class Type>
-void linkedListType<Type>::initializeList()
-{
-	destroyList();
+	if (top == null)
+		return true;
+	else
+		return false;
 }
 template<class Type>
 void linkedListType<Type>::print() const
@@ -70,7 +45,7 @@ void linkedListType<Type>::print() const
 	current = first;
 	while (current != NULL)
 	{
-		cout << current->info << " ";
+		cout << current->data << " ";
 		current - current->link;
 	}
 }
@@ -80,28 +55,94 @@ int linkedListType<Type>::length() const
 	return count;
 }
 template<class Type>
-Type linkedListType<Type>::printTop() const
+void linkedListType<Type>::destroyList()
 {
-	assert(first != NULL);
-	return first->info;
+	nodeType<Type> *tmp;
+	while (first != NULL)
+	{
+		tmp = first;
+		first - first->link;
+		delete tmp;
+	}
+	last = NULL;
+	count = 0;
 }
 template<class Type>
-Type linkedListType<Type>::printBot() const
+Type linkedListType<Type>::Top() const
+{
+	assert(first != NULL);
+	return first->data;
+}
+template<class Type>
+Type linkedListType<Type>::Bot() const
 {
 	assert(last != NULL);
-	return last->info;
+	return last->data;
 }
 template<class Type>
 linkedList<Type> linkedListType<Type>::begin()
 {
-	linkedList<Type> temp(first);
-	return temp;
+	linkedList<Type> tmp(first);
+	return tmp;
 }
 template<class Type>
 linkedList<Type> linkedListType<Type>::end()
 {
-	linkedList<Type> temp(NULL);
-	return temp;
+	linkedList<Type> tmp(NULL);
+	return tmp;
+}
+template<class Type>
+void insertFirst(const Type&) {
+	nodeType<Type> *newNode, *current;
+	if (first != NULL)
+		destroyList();
+	else
+	{
+		current - other.first;
+		first = new nodeType < Type >;
+		first->data = current->data;
+	}
+	count++;
+}
+template<class Type>
+void insertLast(const Type&) {
+	nodeType<Type> *newNode, *current;
+	if (last != NULL)
+		destroyList();
+	else
+	{
+		current - other.last;
+		last = new nodeType < Type >;
+		last->data = current->data;
+	}
+	count++;
+}
+template<class Type>
+void deleteNode(const Type&) {
+
+}
+template<class Type>
+linkedListType<Type>::linkedListType()
+{
+	first = NULL;
+	last = NULL;
+	count = 0;
+}
+template<class Type>
+void linkedListType<Type>::initializeList()
+{
+	destroyList();
+}
+template<class Type>
+linkedListType<Type>::~linkedListType()
+{
+	destroyList();
+}
+template<class Type>
+linkedListType<Type>::linkedListType(const linkedListType<Type>& other)
+{
+	first = NULL;
+	copyList(other);
 }
 template<class Type>
 void linkedListType<Type>::copyList(const linkedListType<Type>& other)
@@ -120,13 +161,13 @@ void linkedListType<Type>::copyList(const linkedListType<Type>& other)
 		current - other.first;
 		count = other.count;
 		first = new nodeType < Type >;
-		first->info = current->info;
+		first->data = current->data;
 		first->link = NULL;
 		current - current->link;
 		while (current != NULL)
 		{
 			newNode = new nodeType < Type >;
-			newNode->info = current->info;
+			newNode->data = current->data;
 			newNode->link = NULL;
 			last->link = newNode;
 			last = newNode;
@@ -134,26 +175,5 @@ void linkedListType<Type>::copyList(const linkedListType<Type>& other)
 		}
 	}
 }
-template<class Type>
-linkedListType<Type>::~linkedListType()
-{
-	destroyList();
-}
-template<class Type>
-linkedListType<Type>::linkedListType(const linkedListType<Type>& other)
-{
-	first = NULL;
-	copyList(other);
-}
-template<class Type>
-const linkedListType<Type>& linkedListType<Type>::operator =(const linkedListType<Type>& other)
-{
-	if (this != &other)
-	{
-		copyList(other);
-	}
-	return *this;
-}
-
 #endif
 
