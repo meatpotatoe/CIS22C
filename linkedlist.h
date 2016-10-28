bb21c2dcda8b2c8e7e8336c3736b229b50f01891
meatpotatoe <evan_a_s2@yahoo.com>
2016-10-27 19:39:06 -0700

    Update linkedlist.h

--- linkedlist.h
+++ linkedlist.h
@@ -3,55 +3,58 @@
 #include <iostream>
 using namespace std;
 template <class Type>
-struct nodeType		//Node member variables 
+struct nodeType  //Node member variables 
 {
-	Type info;	//data
-	nodeType<Type> *link;	//pointer
+	Type data;  //data
+	nodeType<Type> *link;  //pointer
 };
 
 template<class Type>
-class linkedListIterator
+class linkedList
 {
 public:
-	linkedListIterator();
-	linkedListIterator(nodeType<Type>*);
+	linkedList();
+	linkedList(nodeType<Type>*);
 	Type operator*();
-	linkedListIterator<Type> operator++();
-	bool operator==(const linkedListIterator<Type>&) const;
-	bool operator!=(const linkedListIterator<Type>&) const;
+	linkedList<Type> operator++();
+	bool operator==(const linkedList<Type>&) const;
+	bool operator!=(const linkedList<Type>&) const;
 private:
-	nodeType<Type> *current; //pointer to the current node
+	nodeType<Type> *current;  //pointer to the current node
 };
 template <class Type>
-linkedListIterator<Type>::linkedListIterator()	//default constructor
+linkedList<Type>::linkedList()  //default constructor
 {
 	current = NULL;
 }
 
 template <class Type>
-linkedListIterator<Type>::linkedListIterator(nodeType<Type> *ptr)  // constructor
+linkedList<Type>::linkedList(nodeType<Type> *ptr)  // constructor
 {
 	current = ptr;
 }
 template <class Type>
-Type linkedListIterator<Type>::operator*()	//returns data that the pointer points to
+Type linkedList<Type>::operator*()  //returns data that the pointer points to
 {
-	return current->info;
+{
+	return current->data;
 }
 template <class Type>
-linkedListIterator<Type> linkedListIterator<Type>::operator++()	//advances the pointer
+linkedList<Type> linkedList<Type>::operator++()  //advances the pointer
+
 {
 	current = current->link;
 	return *this;
 }
 template <class Type>
-bool linkedListIterator<Type>::operator==(const linkedListIterator<Type>&) const  // checks if current is == to other.current
+bool linkedList<Type>::operator==(const linkedList<Type>&) const  // checks if current is == to other.current
 {
 	return (current == other.current);
 
 }
 template <class Type>
-bool linkedListIterator<Type>::operator!=(const linkedListIterator<Type>&) const  // checks if current is != to other.current
+bool linkedList<Type>::operator!=(const linkedList<Type>&) const  // checks if current is != to other.current
+
 {
 	return (current != other.current);
 }
